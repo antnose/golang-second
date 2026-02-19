18566c3374bb521246558671d69a109956120cc3
antnose <antnose28@gmail.com>
2026-02-19 15:45:46 +0600

    Learn wait group in go routine

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println("Doing tas", id)
 }
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 0; i <= 10; i++ {
-		go task(i)
-
-		go func(i int) {
-			fmt.Println(i)
-		}(i)
+		wg.Add(1)
+		go task(i, &wg)
 	}
 
-	time.Sleep(time.Second)
+	wg.Wait()
+
 }
